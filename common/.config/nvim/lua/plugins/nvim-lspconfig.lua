3ddf40eb689c7846c8aaae91bd94d59e6e27241a
Paul Dino Jones <paul@spacefreak18.xyz>
2025-09-08 15:40:27 -0400

    fix double loading of jdtls

--- common/.config/nvim/lua/plugins/nvim-lspconfig.lua
+++ common/.config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -11,68 +11,30 @@ return {
     -- https://github.com/williamboman/mason-lspconfig.nvim
     { 'williamboman/mason-lspconfig.nvim' },
 
-    -- Auto-Install LSPs, linters, formatters, debuggers
-    -- https://github.com/WhoIsSethDaniel/mason-tool-installer.nvim
-    { 'WhoIsSethDaniel/mason-tool-installer.nvim' },
-
     -- Useful status updates for LSP
     -- https://github.com/j-hui/fidget.nvim
     { 'j-hui/fidget.nvim', opts = {} },
 
     -- Additional lua configuration, makes nvim stuff amazing!
     -- https://github.com/folke/neodev.nvim
-    { 'folke/neodev.nvim', opts = {} },
+    {'folke/neodev.nvim' },
   },
   config = function ()
-    require('mason').setup()
-    require('mason-lspconfig').setup({
-      -- Install these LSPs automatically
-      ensure_installed = {
-        'bashls',
-        'cssls',
-        'html',
-        'gradle_ls',
-        'groovyls',
-        'lua_ls',
-        'jdtls',
-        'jsonls',
-        'lemminx',
-        'marksman',
-        'quick_lint_js',
-        'yamlls',
-      }
-    })
+     require('mason').setup()
+     require('mason-lspconfig').setup({
 
-    require('mason-tool-installer').setup({
-      -- Install these linters, formatters, debuggers automatically
-      ensure_installed = {
-        'java-debug-adapter',
-        'java-test',
-      },
+        automatic_enable = {
+            exclude = {
+                "jdtls",
+            }
+        }
     })
-
-    -- There is an issue with mason-tools-installer running with VeryLazy, since it triggers on VimEnter which has already occurred prior to this plugin loading so we need to call install explicitly
-    -- https://github.com/WhoIsSethDaniel/mason-tool-installer.nvim/issues/39
-    vim.api.nvim_command('MasonToolsInstall')
 
     local lspconfig = require('lspconfig')
     local lsp_capabilities = require('cmp_nvim_lsp').default_capabilities()
     local lsp_attach = function(client, bufnr)
       -- Create your keybindings here...
     end
-
-    -- Call setup on each LSP server
-    require('mason-lspconfig').setup_handlers({
-      function(server_name)
-        -- Don't call setup for JDTLS Java LSP because it will be setup from a separate config
-        if server_name ~= 'jdtls' then
-          lspconfig[server_name].setup({
-            on_attach = lsp_attach,
-            capabilities = lsp_capabilities,
-          })
-        end
-      end
-    })
 
     -- Lua LSP settings
     lspconfig.lua_ls.setup {
@@ -96,4 +58,3 @@ return {
 
   end
 }
-
