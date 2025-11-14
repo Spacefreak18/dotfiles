9d57a4c0c109b8abb4f1668d49b8b19d41c41a00
Paul Dino Jones <paul@spacefreak18.xyz>
2025-11-14 17:13:10 -0500

    add harpoon mappings

--- common/.config/nvim/lua/core/keymaps.lua
+++ common/.config/nvim/lua/core/keymaps.lua
@@ -70,6 +70,8 @@ keymap.set("n", "<leader>gb", ":GitBlameToggle<CR>") -- toggle git blame
 -- Harpoon
 keymap.set("n", "<leader>ha", require("harpoon.mark").add_file)
 keymap.set("n", "<leader>hh", require("harpoon.ui").toggle_quick_menu)
+keymap.set("n", "<leader>hn", require("harpoon.ui").nav_next)
+keymap.set("n", "<leader>hp", require("harpoon.ui").nav_prev)
 keymap.set("n", "<leader>h1", function() require("harpoon.ui").nav_file(1) end)
 keymap.set("n", "<leader>h2", function() require("harpoon.ui").nav_file(2) end)
 keymap.set("n", "<leader>h3", function() require("harpoon.ui").nav_file(3) end)
